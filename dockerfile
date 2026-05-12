1562006118610d983ff3d0c5d2875a976f539ff0
K1err <kleitsmannkeir@gmail.com>
2026-05-12 10:57:36 +0300

    dockerfile

---
+++ dockerfile
@@ -0,0 +1,3 @@
+FROM debian:latest
+
+ENTRYPOINT [ "tail", "-f", "/dev/null" ]
--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@
 import { createServer } from 'node:http';
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Tere maailm!\n');
+  res.end('Tere maailm  !\n');
 });
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
